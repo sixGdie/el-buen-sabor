bde592871862806c2e4c172511822ae5e149774b
sixGdie <sixg.die@gmail.com>
2022-06-27 19:09:58 -0300

    Format

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,163 +1,203 @@
-import NextLink from 'next/link';
-import { AppBar, Badge, Box, IconButton, Link, Toolbar, Typography, Button, Input, InputAdornment } from '@mui/material';
-import { ClearOutlined, SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
-import { useRouter } from 'next/router';
-import { UiContext, CartContext } from '../../context';
-import { useContext, useState } from 'react';
-
+import NextLink from 'next/link'
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+  Button,
+  Input,
+  InputAdornment,
+} from '@mui/material'
+import {
+  ClearOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from '@mui/icons-material'
+import { useRouter } from 'next/router'
+import { UiContext, CartContext } from '../../context'
+import { useContext, useState } from 'react'
 
 export const Navbar = () => {
+  const { asPath, push } = useRouter()
+  const { toggleSideMenu } = useContext(UiContext)
+  const { numberOfItems } = useContext(CartContext)
 
-    const { asPath, push } = useRouter();
-    const { toggleSideMenu } = useContext(UiContext);
-    const { numberOfItems } = useContext(CartContext);
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
 
-    const [searchTerm, setSearchTerm] = useState('')
-    const [isSearchVisible, setIsSearchVisible] = useState(false)
+  const onSearchTerm = () => {
+    if (searchTerm.trim().length === 0) return
+    push(`/search/${searchTerm}`)
+  }
 
-    const onSearchTerm = () => {
-        if (searchTerm.trim().length === 0) return;
-        push(`/search/${searchTerm}`)
-    }
+  return (
+    <AppBar>
+      <Toolbar>
+        <NextLink href='/' passHref>
+          <Link display='flex' alignItems='center' underline='none'>
+            <Typography variant='h6'>El Buen Sabor |</Typography>
+            <Typography sx={{ ml: 0.5 }}>DBZ</Typography>
+          </Link>
+        </NextLink>
 
-    return (
-        <AppBar>
-            <Toolbar>
-                <NextLink href='/' passHref>
-                    <Link display='flex' alignItems='center' underline='none'>
-                        <Typography variant='h6'>El Buen Sabor |</Typography>
-                        <Typography sx={{ ml: 0.5 }}>DBZ</Typography>
-                    </Link>
-                </NextLink>
-                
-                <Box flex={1}/>
+        <Box flex={1} />
 
-                <Box 
-                    sx={{ display: isSearchVisible ? 'none' : {xs: 'none', sm: 'none', md: 'block'}}}
-                    className='fadeIn'
-                >
-                    <NextLink href='/category/bebida' passHref>
-                        <Link underline='none'>
-                            <Button 
-                                style={{backgroundColor: asPath === '/category/bebida' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/bebida' ? 'info' : 'primary'}
-                            >Bebidas</Button>
-                        </Link>
-                    </NextLink>
-                    <NextLink href='/category/hamburguesa' passHref>
-                        <Link underline='none'>
-                        <Button 
-                                style={{backgroundColor: asPath === '/category/hamburguesa' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/hamburguesa' ? 'info' : 'primary'}
-                            >Hamburguesas</Button>
-                        </Link>
-                    </NextLink>
-                    <NextLink href='/category/pizza' passHref>
-                        <Link underline='none'>
-                        <Button 
-                                style={{backgroundColor: asPath === '/category/pizza' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/pizza' ? 'info' : 'primary'}
-                            >Pizzas</Button>
-                        </Link>
-                    </NextLink>
-                    <NextLink href='/category/pancho' passHref>
-                        <Link underline='none'>
-                        <Button 
-                                style={{backgroundColor: asPath === '/category/pancho' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/pancho' ? 'info' : 'primary'}
-                            >Panchos</Button>
-                        </Link>
-                    </NextLink>
-                    <NextLink href='/category/guarnicion' passHref>
-                        <Link underline='none'>
-                        <Button 
-                                style={{backgroundColor: asPath === '/category/guarnicion' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/guarnicion' ? 'info' : 'primary'}
-                            >Guarniciones</Button>
-                        </Link>
-                    </NextLink>
-                    <NextLink href='/category/lomo' passHref>
-                        <Link underline='none'>
-                        <Button 
-                                style={{backgroundColor: asPath === '/category/lomo' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/lomo' ? 'info' : 'primary'}
-                            >Lomos</Button>
-                        </Link>
-                    </NextLink>
-                    <NextLink href='/category/otro' passHref>
-                        <Link underline='none'>
-                        <Button 
-                                style={{backgroundColor: asPath === '/category/otro' ? 'black' : 'primary'
-                                 }} color={asPath === '/category/otro' ? 'info' : 'primary'}
-                            >Otros</Button>
-                        </Link>
-                    </NextLink>
-                </Box>
+        <Box
+          sx={{
+            display: isSearchVisible
+              ? 'none'
+              : { xs: 'none', sm: 'none', md: 'block' },
+          }}
+          className='fadeIn'
+        >
+          <NextLink href='/category/bebida' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/bebida' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/bebida' ? 'info' : 'primary'}
+              >
+                Bebidas
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href='/category/hamburguesa' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/hamburguesa' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/hamburguesa' ? 'info' : 'primary'}
+              >
+                Hamburguesas
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href='/category/pizza' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/pizza' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/pizza' ? 'info' : 'primary'}
+              >
+                Pizzas
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href='/category/pancho' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/pancho' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/pancho' ? 'info' : 'primary'}
+              >
+                Panchos
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href='/category/guarnicion' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/guarnicion' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/guarnicion' ? 'info' : 'primary'}
+              >
+                Guarniciones
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href='/category/lomo' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/lomo' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/lomo' ? 'info' : 'primary'}
+              >
+                Lomos
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href='/category/otro' passHref>
+            <Link underline='none'>
+              <Button
+                style={{
+                  backgroundColor:
+                    asPath === '/category/otro' ? 'black' : 'primary',
+                }}
+                color={asPath === '/category/otro' ? 'info' : 'primary'}
+              >
+                Otros
+              </Button>
+            </Link>
+          </NextLink>
+        </Box>
 
-                <Box flex={1}/>
+        <Box flex={1} />
 
-                
-
-                {
-                    isSearchVisible
-                        ? (
-                            <Input
-                                sx={{ display: {xs: 'none', sm: 'flex'}}}
-                                className='fadeIn'
-                                autoFocus
-                                value={ searchTerm }
-                                onChange={ (e) => setSearchTerm(e.target.value) }
-                                onKeyPress={ (e) => e.key === 'Enter' ? onSearchTerm() : null }
-                                type='text'
-                                placeholder="Buscar..."
-                                endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                            onClick={ () => setIsSearchVisible(false) }
-                                        >
-                                        <ClearOutlined/>
-                                    </IconButton>
-                                </InputAdornment>
-                                }
-                            />
-                        )
-                        :
-                        (
-                            <IconButton
-                                onClick={ () => setIsSearchVisible(true) }
-                                className='fadeIn'
-                                sx={{ display: {xs: 'none', sm: 'flex'}}}
-                            >
-                                <SearchOutlined/>
-                            </IconButton>
-                        )
-
-                }
-
-                
-
-                <IconButton
-                    sx={{ display: {xs: 'flex', sm: 'none'}}}
-                    onClick={toggleSideMenu}
-                >
-                    <SearchOutlined/>
+        {isSearchVisible ? (
+          <Input
+            sx={{ display: { xs: 'none', sm: 'flex' } }}
+            className='fadeIn'
+            autoFocus
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={(e) => (e.key === 'Enter' ? onSearchTerm() : null)}
+            type='text'
+            placeholder='Buscar...'
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton onClick={() => setIsSearchVisible(false)}>
+                  <ClearOutlined />
                 </IconButton>
+              </InputAdornment>
+            }
+          />
+        ) : (
+          <IconButton
+            onClick={() => setIsSearchVisible(true)}
+            className='fadeIn'
+            sx={{ display: { xs: 'none', sm: 'flex' } }}
+          >
+            <SearchOutlined />
+          </IconButton>
+        )}
 
-                <NextLink href="/cart" passHref>
-                    <Link underline='none'>
-                        <IconButton>
-                            <Badge badgeContent={numberOfItems > 9 ? '+9' : numberOfItems} color='secondary'>
-                                <ShoppingCartOutlined/>
-                            </Badge>
-                        </IconButton>
-                    </Link>
-                </NextLink>
+        <IconButton
+          sx={{ display: { xs: 'flex', sm: 'none' } }}
+          onClick={toggleSideMenu}
+        >
+          <SearchOutlined />
+        </IconButton>
 
-                <Button onClick={toggleSideMenu}>
-                    Menú
-                </Button>
+        <NextLink href='/cart' passHref>
+          <Link underline='none'>
+            <IconButton>
+              <Badge
+                badgeContent={numberOfItems > 9 ? '+9' : numberOfItems}
+                color='secondary'
+              >
+                <ShoppingCartOutlined />
+              </Badge>
+            </IconButton>
+          </Link>
+        </NextLink>
 
-            </Toolbar>
-        </AppBar>
-    )
+        <Button onClick={toggleSideMenu}>Menú</Button>
+      </Toolbar>
+    </AppBar>
+  )
 }
--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -1,19 +1,17 @@
-import { createTheme } from '@mui/material/styles';
-import { red,  } from '@mui/material/colors';
-
+import { createTheme } from '@mui/material/styles'
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#1E1E1E'
+      main: '#1E1E1E',
     },
     secondary: {
-      main: '#3A64D8'
+      main: '#3A64D8',
     },
     info: {
-      main: '#fff'
-    }
+      main: '#fff',
+    },
   },
 
   components: {
@@ -30,28 +28,27 @@ export const lightTheme = createTheme({
       styleOverrides: {
         root: {
           backgroundColor: 'white',
-          height: 60
+          height: 60,
         },
-      }
+      },
     },
 
     MuiTypography: {
       styleOverrides: {
         h1: {
           fontSize: 30,
-          fontWeight: 600
+          fontWeight: 600,
         },
         h2: {
           fontSize: 20,
-          fontWeight: 400
+          fontWeight: 400,
         },
         subtitle1: {
           fontSize: 18,
-          fontWeight: 600
-        }
-      }
+          fontWeight: 600,
+        },
+      },
     },
-
 
     MuiButton: {
       defaultProps: {
@@ -59,31 +56,30 @@ export const lightTheme = createTheme({
         size: 'small',
         disableElevation: true,
       },
-      
+
       styleOverrides: {
         root: {
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
-          ":hover": {
+          ':hover': {
             backgroundColor: 'rgba(0,0,0,0.25)',
-            transition: 'all 0.3s ease-in-out'
-          }
-        }
-      }
+            transition: 'all 0.3s ease-in-out',
+          },
+        },
+      },
     },
-
 
     MuiCard: {
       defaultProps: {
-        elevation: 0
+        elevation: 0,
       },
       styleOverrides: {
         root: {
           boxShadow: '0px 5px 5px rgba(0,0,0,0.05)',
           borderRadius: '10px',
-        }
-      }
-    } 
-  }
-});
+        },
+      },
+    },
+  },
+})
